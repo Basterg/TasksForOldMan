5aeb60abb1042384bb32ecdd4506b9d3066904f8
Basterg <46724354+basterg@users.noreply.github.com>
2019-01-16 15:31:39 +0500

    1t commit

---
+++ task07/task0713/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task07.task0713;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        initializeArray(arrayList);
+        ArrayList<Integer> arrayListMultipleOf2 = new ArrayList<>();
+        ArrayList<Integer> arrayListMultipleOf3 = new ArrayList<>();
+        ArrayList<Integer> arrayList1Other = new ArrayList<>();
+        multiplicitySorting(arrayList, arrayListMultipleOf2, arrayListMultipleOf3, arrayList1Other);
+        printList(arrayListMultipleOf2, arrayListMultipleOf3, arrayList1Other);
+    }
+
+    public static void initializeArray(ArrayList<Integer> arrayList) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+    }
+
+    public static void multiplicitySorting(ArrayList<Integer> a, ArrayList<Integer> a2,
+                           ArrayList<Integer> a3, ArrayList<Integer> aO) {
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) % 2 == 0 && a.get(i) % 3 == 0) {
+                a2.add(a.get(i));
+                a3.add(a.get(i));
+            } else if (a.get(i) % 2 == 0) {
+                a2.add(a.get(i));
+            } else if (a.get(i) % 3 == 0) {
+                a3.add(a.get(i));
+            } else {
+                aO.add(a.get(i));
+            }
+        }
+    }
+
+    public static void printList(ArrayList<Integer> a2, ArrayList<Integer> a3, ArrayList<Integer> aO) {
+        for (int i = 0; i < a2.size(); i++) {
+            System.out.println(a2.get(i));
+        }
+        for (int i = 0; i < a3.size(); i++) {
+            System.out.println(a3.get(i));
+        }
+        for (int i = 0; i < aO.size(); i++) {
+            System.out.println(aO.get(i));
+        }
+    }
+}
