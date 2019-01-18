05f6d7f6526a866210aae3ef5214e48db3019794
basterg <sitxbasterg@inbox.ru>
2019-01-18 19:32:15 +0500

    mb tak

--- task07/task0713/Solution.java
+++ task07/task0713/Solution.java
@@ -7,26 +7,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    static ArrayList<Integer> arrayList = new ArrayList<>();
-    static ArrayList<Integer> arrayListMultipleOf2 = new ArrayList<>();
-    static ArrayList<Integer> arrayListMultipleOf3 = new ArrayList<>();
-    static ArrayList<Integer> arrayList1Other = new ArrayList<>();
+    static private ArrayList<Integer> a = new ArrayList<>();
+    //a - список, который мы заполним 20тью целыми числами
+    static private ArrayList<Integer> a2 = new ArrayList<>();
+    //a2 - список, который мы заполним числами кратными двум, которые содержатся в списке "а"
+    static private ArrayList<Integer> a3 = new ArrayList<>();
+    //a3 - список, который мы заполним числами кратными трём, которые содержатся в списке "а"
+    static private ArrayList<Integer> aO = new ArrayList<>();
+    //aO - список, который мы заполним числами не кратными трём и двум, которые содержатся в списке "а"
 
     public static void main(String[] args) throws Exception {
-        initializeArray(arrayList);
-        multiplicitySorting(arrayList, arrayListMultipleOf2, arrayListMultipleOf3, arrayList1Other);
-        printList(arrayListMultipleOf2, arrayListMultipleOf3, arrayList1Other);
+        initializeArray();
+        multiplicitySorting();
+        printList();
     }
 
-    public static void initializeArray(ArrayList<Integer> arrayList) throws IOException {
+    public static void initializeArray() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 20; i++) {
-            arrayList.add(Integer.parseInt(reader.readLine()));
+            a.add(Integer.parseInt(reader.readLine()));
         }
     }
 
-    public static void multiplicitySorting(ArrayList<Integer> a, ArrayList<Integer> a2,
-                           ArrayList<Integer> a3, ArrayList<Integer> aO) {
+    public static void multiplicitySorting() {
         for (int i = 0; i < a.size(); i++) {
             if (a.get(i) % 2 == 0 && a.get(i) % 3 == 0) {
                 a2.add(a.get(i));
@@ -41,7 +44,7 @@ public class Solution {
         }
     }
 
-    public static void printList(ArrayList<Integer> a2, ArrayList<Integer> a3, ArrayList<Integer> aO) {
+    public static void printList() {
         for (int i = 0; i < a2.size(); i++) {
             System.out.println(a2.get(i));
         }
