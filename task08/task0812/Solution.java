7f76ba171fb8b0750e5354c8da995690fc60ca0d
basterg <sitxbasterg@inbox.ru>
2019-01-19 16:33:16 +0500

    1st commit

---
+++ task08/task0812/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0812;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Cамая длинная последовательность
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        initializeArray(arrayList);
+        printLengthOfLongestSequence(arrayList);
+    }
+
+    public static void initializeArray(ArrayList<Integer> arrayList) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+    }
+
+    public static void printLengthOfLongestSequence(ArrayList<Integer> arrayList) {
+        int count = 1; // Счётчик самой длинной последовательности
+        int addCount = 1; // Дополнительный счётчик (буферный)
+        for (int i = 0; i < 9; i++) {
+            int element = arrayList.get(i);
+            int nextElement = arrayList.get(i + 1);
+            if (element == nextElement) {
+                addCount += 1;
+            } else if (addCount > count) {
+                count = addCount;
+                addCount = 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
