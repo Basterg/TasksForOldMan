0ae159b8f8f20654f4abcacd4f6de9cea56a982f
basterg <sitxbasterg@inbox.ru>
2019-01-20 21:15:19 +0500

    1st commit

--- task08/task0819/Solution.java
+++ task08/task0819/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task08.task0819;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /* 
@@ -10,20 +12,36 @@ Set из котов
 public class Solution {
     public static void main(String[] args) {
         Set<Cat> cats = createCats();
-
-        //напишите тут ваш код. step 3 - пункт 3
-
+        removeOneElement(cats);
         printCats(cats);
     }
 
     public static Set<Cat> createCats() {
-        //напишите тут ваш код. step 2 - пункт 2
-        return null;
+        Set<Cat> cats = new HashSet<>();
+        Cat cat1 = new Cat();
+        cats.add(cat1);
+        Cat cat2 = new Cat();
+        cats.add(cat2);
+        Cat cat3 = new Cat();
+        cats.add(cat3);
+
+        return cats;
     }
 
     public static void printCats(Set<Cat> cats) {
-        // step 4 - пункт 4
+        for (Cat cat : cats) {
+            System.out.println(cat);
+        }
     }
 
-    // step 1 - пункт 1
+    public static class Cat {
+
+    }
+
+    public static void removeOneElement(Set<Cat> cats) {
+        Iterator<Cat> iterator = cats.iterator();
+        if (iterator.hasNext()) {
+            cats.remove(iterator.next());
+        }
+    }
 }
