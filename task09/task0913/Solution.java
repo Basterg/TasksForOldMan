cc30b0e973bc09ba1400f55b39668541d1e28dde
basterg <sitxbasterg@inbox.ru>
2019-01-22 21:32:58 +0500

    1st commit

--- task09/task0913/Solution.java
+++ task09/task0913/Solution.java
@@ -9,11 +9,13 @@ import java.net.URISyntaxException;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-
-        method1();
-
-        //напишите тут ваш код
+        try {
+            method1();
+        }
+        catch (NullPointerException e) {
+        }
+        catch (FileNotFoundException e) {
+        }
     }
 
     public static void method1() throws NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException {
