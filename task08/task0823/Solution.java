181b9f1bd42bbac3a6dcee8e6d166cc0b2c228fe
basterg <sitxbasterg@inbox.ru>
2019-01-23 16:53:14 +0500

    1st commit

--- task08/task0823/Solution.java
+++ task08/task0823/Solution.java
@@ -11,8 +11,20 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String s = reader.readLine();
+        String string = reader.readLine();
+        firstLetersToUpperCase(string);
+    }
 
-        //напишите тут ваш код
+    public static void firstLetersToUpperCase(String string) {
+        char[] charsOfString = string.toCharArray();
+        System.out.print(Character.toString(charsOfString[0]).toUpperCase());
+        for (int i = 1; i < charsOfString.length; i++) {
+            String leftElement = Character.toString(charsOfString[i - 1]);
+            String element = Character.toString(charsOfString[i]);
+            if (leftElement.equals(" ") && !element.equals(" ")) {
+                element = element.toUpperCase();
+            }
+            System.out.print(element);
+        }
     }
 }
