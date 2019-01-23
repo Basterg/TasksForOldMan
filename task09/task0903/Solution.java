194d89ae484da015fd49f07438fc93100340e293
basterg <sitxbasterg@inbox.ru>
2019-01-23 14:00:41 +0500

    1st correction

--- task09/task0903/Solution.java
+++ task09/task0903/Solution.java
@@ -11,29 +11,29 @@ public class Solution {
 
     public static int method1() {
         method2();
-        StackTraceElement[] stackTraceElementsMethod4 = Thread.currentThread().getStackTrace();
-        return stackTraceElementsMethod4[2].getLineNumber();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getLineNumber();
     }
 
     public static int method2() {
         method3();
-        StackTraceElement[] stackTraceElementsMethod4 = Thread.currentThread().getStackTrace();
-        return stackTraceElementsMethod4[2].getLineNumber();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getLineNumber();
     }
 
     public static int method3() {
         method4();
-        StackTraceElement[] stackTraceElementsMethod4 = Thread.currentThread().getStackTrace();
-        return stackTraceElementsMethod4[2].getLineNumber();    }
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getLineNumber();    }
 
     public static int method4() {
         method5();
-        StackTraceElement[] stackTraceElementsMethod4 = Thread.currentThread().getStackTrace();
-        return stackTraceElementsMethod4[2].getLineNumber();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getLineNumber();
     }
 
     public static int method5() {
-        StackTraceElement[] stackTraceElementsMethod4 = Thread.currentThread().getStackTrace();
-        return stackTraceElementsMethod4[2].getLineNumber();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getLineNumber();
     }
 }
