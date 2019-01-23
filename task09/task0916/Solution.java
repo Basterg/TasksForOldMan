583eed9ec2417be03b9f39b26160692f8da195e7
basterg <sitxbasterg@inbox.ru>
2019-01-23 23:34:40 +0500

    1st commit

--- task09/task0916/Solution.java
+++ task09/task0916/Solution.java
@@ -14,9 +14,24 @@ public class Solution {
     }
 
     public static void handleExceptions(Solution obj) {
-        obj.method1();
-        obj.method2();
-        obj.method3();
+        try {
+            obj.method1();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        try {
+            obj.method2();
+        }
+        catch (NoSuchFieldException e) {
+            System.out.println(e);
+        }
+        try {
+            obj.method3();
+        }
+        catch (RemoteException e) {
+            System.out.println(e);
+        }
     }
 
     public void method1() throws IOException {
