4b76c7fa0ceb7f7ddf3d11ce3ebb3b16acacd555
basterg <sitxbasterg@inbox.ru>
2019-01-25 00:43:17 +0500

    1st correction

--- task08/task0826/Solution.java
+++ task08/task0826/Solution.java
@@ -25,7 +25,7 @@ public class Solution {
     }
 
     public static void sort(int[] array) {
-        for (int i = 0; i < 19; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int max = array[i];
             int indexOfMax = i;
             for (int j = i + 1; j < 20; j++) {
