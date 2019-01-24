64bafae3e022339d951e4e525450e39d58339fc3
basterg <sitxbasterg@inbox.ru>
2019-01-24 16:48:19 +0500

    1st commit

--- task09/task0926/Solution.java
+++ task09/task0926/Solution.java
@@ -13,7 +13,19 @@ public class Solution {
     }
 
     public static ArrayList<int[]> createList() {
-        //напишите тут ваш код
+        ArrayList<int[]> list = new ArrayList<>();
+        int[] firstArray = {1, 2, 3, 3, 5};
+        list.add(firstArray);
+        int[] secondArray = {2, 2};
+        list.add(secondArray);
+        int[] thirdArray = {1, 2, 3, 4};
+        list.add(thirdArray);
+        int[] fourthArray = {1, 2, 3, 3, 5, 3, 8};
+        list.add(fourthArray);
+        int[] fifthArray = {};
+        list.add(fifthArray);
+
+        return list;
     }
 
     public static void printList(ArrayList<int[]> list) {
