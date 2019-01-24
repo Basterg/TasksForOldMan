17543f16ef1c91e093339303cef1217a4d414cc4
basterg <sitxbasterg@inbox.ru>
2019-01-24 15:38:34 +0500

    1st commit

--- task09/task0921/Solution.java
+++ task09/task0921/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task09.task0921;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,23 @@ import java.util.List;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         readData();
     }
 
-    public static void readData() {
-        //напишите тут ваш код
+    public static void readData() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; true; i++) {
+            String string = reader.readLine();
+            try {
+                int number = Integer.parseInt(string);
+                list.add(number);
+            } catch (NumberFormatException e) {
+                for (int j : list) {
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
