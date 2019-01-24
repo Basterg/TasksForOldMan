3c72326df71390f84b42aab0b00935b22bd9d102
basterg <sitxbasterg@inbox.ru>
2019-01-24 15:15:05 +0500

    1st commit

--- task09/task0919/Solution.java
+++ task09/task0919/Solution.java
@@ -4,9 +4,20 @@ package com.javarush.task.task09.task0919;
 Деление на ноль
 */
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void main(String[] args) {
-        divideByZero();
+        try {
+            divideByZero();
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
+
+    public static void divideByZero() {
+        System.out.println(47 / 0);
+    }
+
 }
