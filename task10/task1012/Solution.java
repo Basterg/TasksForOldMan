5111ef378d9f818df9ced0fb4f30f9ec8f012209
basterg <sitxbasterg@inbox.ru>
2019-01-25 21:05:26 +0500

    1st commit

--- task10/task1012/Solution.java
+++ task10/task1012/Solution.java
@@ -2,7 +2,9 @@ package com.javarush.task.task10.task1012;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /* 
 Количество букв
@@ -16,20 +18,35 @@ public class Solution {
         String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         char[] abcArray = abc.toCharArray();
 
-        ArrayList<Character> alphabet = new ArrayList<Character>();
+        ArrayList<Character> alphabet = new ArrayList<>();
         for (int i = 0; i < abcArray.length; i++) {
             alphabet.add(abcArray[i]);
         }
 
         // Ввод строк
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             String s = reader.readLine();
             list.add(s.toLowerCase());
         }
 
+        String newString = "";
+        for (int i =0; i < list.size(); i++) {
+            newString += list.get(i);
+        }
 
-        // напишите тут ваш код
+        char[] charsOfString = newString.toCharArray();
+        for (int j = 0; j < alphabet.size(); j++) {
+            int count = 0;
+            Character currentLetter = alphabet.get(j);
+            for (int k = 0; k < charsOfString.length; k++) {
+                Character currentChar = charsOfString[k];
+                if (currentLetter.equals(currentChar)) {
+                    count += 1;
+                }
+            }
+            System.out.println(currentLetter + " " + count);
+        }
     }
 
 }
