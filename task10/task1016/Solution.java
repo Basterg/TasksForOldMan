9a0cba359b723afc54b7bdf9ad5d562a663fa571
basterg <sitxbasterg@inbox.ru>
2019-01-27 19:12:18 +0500

    1st commit

--- task10/task1016/Solution.java
+++ task10/task1016/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<>();
         for (int i = 0; i < 20; i++) {
             words.add(reader.readLine());
         }
@@ -26,12 +26,27 @@ public class Solution {
         }
     }
 
-    public static Map<String, Integer> countWords(ArrayList<String> list) {
-        HashMap<String, Integer> result = new HashMap<String, Integer>();
+    public static Map<String, Integer> countWords(ArrayList<String> words) {
+        HashMap<String, Integer> result = new HashMap();
+        /* Заполняем мапу словами, затем переводим ключи мапы в список,
+        таки образом, у нас есть список слов без их копий
+         */
 
-        //напишите тут ваш код
+        for (int i = 0; i < words.size(); i++) {
+            result.put(words.get(i), 0);
+        }
 
-
+        ArrayList<String> listOfKeys = new ArrayList<>(result.keySet());
+        for (int i = 0; i < listOfKeys.size(); i++) {
+            int count = 1;
+            String currentWord = listOfKeys.get(i);
+            for (int j = 0; j < words.size(); j++) {
+                if (currentWord.equals(words.get(j))) {
+                    count++;
+                }
+            }
+            result.put(currentWord, count);
+        }
         return result;
     }
 
