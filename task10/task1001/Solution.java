119aaa0456399cf19da321065aa45a57ffb78f27
basterg <sitxbasterg@inbox.ru>
2019-01-28 15:31:21 +0500

    1st correction

--- task10/task1001/Solution.java
+++ task10/task1001/Solution.java
@@ -7,10 +7,10 @@ package com.javarush.task.task10.task1001;
 public class Solution {
     public static void main(String[] args) {
         int a = 0;
-        int b = (byte) a + 46;
+        int b = a + 46;
         byte c = (byte) (a * b);
-        double f = (char) 1234.15;
-        long d = (long) (a + f / c + b);
+        double f = 1234.15;
+        long d = (a + (byte)(f / c) + b);
         System.out.println(d);
     }
 }
