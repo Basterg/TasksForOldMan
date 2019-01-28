fe5f1b59f7e50ea27dde84ac6596982b733f8670
basterg <sitxbasterg@inbox.ru>
2019-01-28 15:41:13 +0500

    1st correction

--- task10/task1005/Solution.java
+++ task10/task1005/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.task10.task1005;
 
 public class Solution {
     public static void main(String[] args) {
-        int a = (byte) 44;
+        int a = 44;
         int b = 300;
         short c = (short) (b - a);
         System.out.println(c);
